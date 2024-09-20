dccdbea4138ac48ff61747ef8c626f9b299b9173
Majd <majdlouka4@gmail.com>
2024-09-20 15:17:16 +0300

    added new accounts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,8 +14,28 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create([
-            'username' => 'sarmadi',
-            'password' => Hash::make('P@ssword1')
+            'username' => 'admin1',
+            'password' => Hash::make('admin1@taatufnvc')
+        ]);
+
+        \App\Models\User::factory()->create([
+            'username' => 'admin2',
+            'password' => Hash::make('admin2@taatufnvc')
+        ]);
+
+        \App\Models\User::factory()->create([
+            'username' => 'admin3',
+            'password' => Hash::make('admin3@taatufnvc')
+        ]);
+
+        \App\Models\User::factory()->create([
+            'username' => 'admin4',
+            'password' => Hash::make('admin4@taatufnvc')
+        ]);
+        
+        \App\Models\User::factory()->create([
+            'username' => 'admin5',
+            'password' => Hash::make('admin5@taatufnvc')
         ]);
     }
 }
